58b73b4e2d13ed1c7f872f3a3857a1a424e4634f
zarifatb-15 <zarifatb@code.edu.az>
2026-06-21 23:50:34 +0400

    chore: generated clean InitialIdentitySetup migration without cascade errors

--- src/Infrastructure/OnlineAuctionApp.Persistence/Configurations/AuctionConfiguration.cs
+++ src/Infrastructure/OnlineAuctionApp.Persistence/Configurations/AuctionConfiguration.cs
@@ -3,20 +3,21 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using OnlineAuctionApp.Domain.Entities;
 
 namespace OnlineAuctionApp.Persistence.Configurations;
+
 public class AuctionConfiguration : IEntityTypeConfiguration<Auction>
 {
-    public void Configure(EntityTypeBuilder<Auction> builder)
-    {
-        builder.Property(a => a.StartingPrice)
-               .HasColumnType("decimal(18,2)");
+       public void Configure(EntityTypeBuilder<Auction> builder)
+       {
+              builder.Property(a => a.StartingPrice)
+                     .HasColumnType("decimal(18,2)");
 
-        builder.Property(a => a.CurrentPrice)
-               .HasColumnType("decimal(18,2)");
+              builder.Property(a => a.CurrentPrice)
+                     .HasColumnType("decimal(18,2)");
 
 
-        builder.HasOne(a => a.Seller)
-               .WithMany()
-               .HasForeignKey(a => a.SellerId)
-               .OnDelete(DeleteBehavior.Restrict);
-    }
+              builder.HasOne(a => a.Seller)
+                     .WithMany()
+                     .HasForeignKey(a => a.SellerId)
+                     .OnDelete(DeleteBehavior.Restrict);
+       }
 }
