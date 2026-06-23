82ab8b6a2c6d38bd2fae369839f464167d56a25a
zarifatb-15 <zarifatb@code.edu.az>
2026-06-23 21:39:25 +0400

    feat: added RegisterDto for user registration process

---
+++ src/Core/OnlineAuctionApp.Application/DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,11 @@
+namespace OnlineAuctionApp.Application.DTOs.Auth;
+
+public class RegisterDto
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string ConfirmPassword { get; set; } = null!;
+}
